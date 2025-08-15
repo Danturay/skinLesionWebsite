5a419ee3b3eb09e6ea02f4b265e3127a4bf46e9d
Dante Turay <dante.turay@yahoo.co.uk>
2025-08-15 14:33:13 +0100

    Fixed bug for render build

--- src/sections/NavLink.tsx
+++ src/sections/NavLink.tsx
@@ -4,16 +4,18 @@ const NavLink = ({
   href,
   title,
   onClick,
+  className,
 }: {
   href: string;
   title: string;
   onClick?: () => void;
+  className?: string;
 }) => {
   return (
     <Link
       href={href}
-      onClick={onClick} // ✅ Now supported
-      className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
+      onClick={onClick}
+      className={`block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white ${className}`}
     >
       {title}
     </Link>
